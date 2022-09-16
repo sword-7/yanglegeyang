1c3150c26ebe7707b257cdacc8ae5a3bd8ffe80a
晚来天欲雪 <fujf1@yonyou.com>
2022-09-17 00:57:00 +0800

    优化执行次数

--- src/main/java/sheep.java
+++ src/main/java/sheep.java
@@ -1,36 +1,38 @@
 import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient.Builder;
 import okhttp3.Request;
 import okhttp3.Response;
 import util.HttpClient;
 import util.RandomUtil;
 
-import okhttp3.OkHttpClient.Builder;
 import java.io.IOException;
 
 public class sheep {
 
 	// 替换你的token
-	private static String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTQzOTY2NjgsIm5iZiI6MTY2MzI5NDQ2OCwiaWF0IjoxNjYzMjkyNjY4LCJqdGkiOiJDTTpjYXRfbWF0Y2g6bHQxMjM0NTYiLCJvcGVuX2lkIjoiIiwidWlkIjoyMDcyOTQ5OTcsImRlYnVnIjoiIiwibGFuZyI6IiJ9.NVap_hjF66D8HQzzu6R9ImME1zH-BgNIR_wr8PKz1Ik";
-	private static Builder client = HttpClient.client;
+	private final static String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTQyNzU0MjIsIm5iZiI6MTY2MzE3MzIyMiwiaWF0IjoxNjYzMTcxNDIyLCJqdGkiOiJDTTpjYXRfbWF0Y2g6bHQxMjM0NTYiLCJvcGVuX2lkIjoiIiwidWlkIjo1MjI5NTAyOSwiZGVidWciOiIiLCJsYW5nIjoiIn0.BlyAs5rMFb0kANZpGCZea0y9Zu91yWbATrlPpmKD45g";
+	private final static Builder client = HttpClient.client;
 
 	public static void main(String[] args) {
-		try {
-			System.out.println("【羊了个羊一键闯关开始启动】");
-			int cycle_count = 100;
-			for (int i = 0; i < cycle_count; i++) {
-				System.out.println("...第"+(i + 1)+"开始完成闯关...");
-				int cost_time = RandomUtil.getRandom(1, 3600);
-				System.out.println("生成随机完成耗时:"+cost_time);
-				finish_game(1, cost_time);
-				System.out.println("(【羊了个羊一键闯关开始结束】"+cost_time);
-				Thread.sleep(300L);
+		System.out.println("【羊了个羊一键闯关开始启动】");
+		int cycle_count = 10;
+		for (int i=0; i<cycle_count; i++) {
+			System.out.println("...开始完成第"+(i + 1)+"次闯关...");
+			int cost_time = RandomUtil.getRandom(1, 3600);
+			System.out.println("生成随机完成耗时:"+cost_time);
+			if(finish_game(1, cost_time)){
+				System.out.println("...完成第"+(i + 1)+"次闯关，闯关成功...");
+				System.out.println("(【羊了个羊一键闯关结束】"+cost_time);
+				break;
 			}
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+			if(i==9){
+				System.out.println("【羊了个羊闯关失败...】");
+			}
+			System.out.println("(【羊了个羊第"+(i + 1)+"次闯关失败，重试中...】"+cost_time);
 		}
 	}
 
-	public static void finish_game(int state, int rank_time) {
+	public static boolean finish_game(int state, int rank_time) {
 		try {
 			String finish_api = "https://cat-match.easygame2021.com/sheep/v1/game/game_over?rank_score=1&rank_state=%s&rank_time=%s&rank_role=1&skin=1";
 			Request.Builder reqBuild = new Request.Builder();
@@ -47,8 +49,10 @@ public class sheep {
 					System.out.println("请求不成功===>"+response.toString());
 				}
 				System.out.println("执行成功===>"+response.body().string());
+				return true;
 			} catch (IOException e) {
 				System.out.println("===>请求超时，服务器太多请求了，偶尔超时很正常");
+				return false;
 			} finally {
 				if (response != null) {
 					response.close();
@@ -57,6 +61,6 @@ public class sheep {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
+		return false;
 	}
 }
